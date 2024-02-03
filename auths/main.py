21db95043b725630ceeaa724f60aa2267bf9317a
Neivanny1 <nevileneivanny@gmail.com>
2024-02-03 06:26:01 +0000

    login user interface

--- auths/main.py
+++ auths/main.py
@@ -7,12 +7,12 @@ from handle import get_db_uri
 app = Flask(__name__)
 creds = get_db_uri()
 # Change this to your secret key (it can be anything, it's for extra protection)
-app.secret_key = 'your secret key'
+app.secret_key = creds[3]
 
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = creds[0]
 app.config['MYSQL_USER'] = creds[1]
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = creds[2]
 app.config['MYSQL_DB'] = creds=[1]
 
 # Intialize MySQL
